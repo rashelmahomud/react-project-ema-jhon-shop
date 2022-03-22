be3c6ada28c75239dc139b353d2965f7d087ea3d
rashelmahomud <rashelmahmudraj1998@gmail.com>
2022-03-23 05:19:41 +0600

    added shoping cart some

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Cart.css';
+import '@fortawesome/react-fontawesome';
+import '@fortawesome/free-solid-svg-icons';
 
-const Cart = ({cart}) => {
+const Cart = (props) => {
+    const {cart} = props;
+    console.log(cart);
+    let total = 0;
+    let shipping = 0;
+    for(const product of cart){
+        total = total + product.price;
+        shipping = shipping + product.shipping;
+
+    }
     return (
         <div className='cart-site'>
                 <h4>Order Summery Here</h4>
                 <p>Select items: {cart.length}</p>
+                <p>Total Price: ${total}</p>
+                <p>Total Shipping Charge: ${shipping}</p>
+                <p>Tax: $</p>
+                <h4>Grand Total: $</h4>
+                <button className='clear-btn'>Clear Cart</button>
+                
+                <button className='review-btn'>Review Order</button>
                 
         </div>
     );
